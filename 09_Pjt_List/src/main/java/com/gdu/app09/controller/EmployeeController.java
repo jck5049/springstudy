35aef2242b545743800634fc81d46b446239cacc
jck5049 <jck5049@naver,com>
2023-04-26 17:47:14 +0900

    2023-04-26

--- 09_Pjt_List/src/main/java/com/gdu/app09/controller/EmployeeController.java
+++ 09_Pjt_List/src/main/java/com/gdu/app09/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.gdu.app09.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -8,12 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gdu.app09.service.EmployeeListService;
 
 @Controller
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeListService employeeListService;
 	
@@ -23,14 +26,23 @@ public class EmployeeController {
 		return "employees/pagination";
 	}
 	
-	
 	@GetMapping("/employees/change/record.do")
 	public String changeRecord(HttpSession session
-							 , HttpServletRequest request
-							 , @RequestParam(value="recordPerPage", required=false, defaultValue="10") int recordPerPage) {
+			                 , HttpServletRequest request
+			                 , @RequestParam(value="recordPerPage", required=false, defaultValue="10") int recordPerPage) {
 		session.setAttribute("recordPerPage", recordPerPage);
-		return "redirect:" + request.getHeader("referer");	// 현재 주소(/employees/change/record.do)의 이전 주소(Referer)로 이동하시오.
+		return "redirect:" + request.getHeader("referer");  // 현재 주소(/employees/change/record.do)의 이전 주소(Referer)로 이동하시오.
 	}
 	
+	@GetMapping("/employees/scroll.page")
+	public String scrollPage() {
+		return "employees/scroll";
+	}
+	
+	@ResponseBody	// 이코드로 인해 jsp로 이동이 아니라 a.jax로 이동하게 해주는 코드이다.
+	@GetMapping(value="/employee/scroll.do", produces="application/json")
+	public Map<String, Object> scroll(HttpServletRequest request) {
+		return employeeListService.getEmployeeListUsingScroll(request);
+	}
 	
 }
--- 09_Pjt_List/src/main/webapp/WEB-INF/views/index.jsp
+++ 09_Pjt_List/src/main/webapp/WEB-INF/views/index.jsp
@@ -2,7 +2,7 @@
     pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
 <c:set var="contextPath" value="${pageContext.request.contextPath}" />
 <!DOCTYPE html>
 <html>
@@ -16,7 +16,9 @@
 	<div>
 		<a href="${contextPath}/employees/pagination.do">페이징</a>
 	</div>
-	
+	<div>
+		<a href="${contextPath}/employees/scroll.page">스크롤</a>
+	</div>
 	
 </body>
 </html>
